8296d61a19d680755fb0ac52195657c3b9b84b3a
nbvnbv <nbv3nbv@yahoo.com>
2024-07-01 18:49:24 +0300

    relay connect --name my-tunnel :8008

--- server.py
+++ server.py
@@ -139,7 +139,7 @@ def run_localhostrun():
 
 def run_hrzn():
     hrzn_process = subprocess.Popen(
-        ["relay", "connect", "--name", "nbvnbv"],
+        ["relay", "connect", "--name", "nbvnbv", ":8008"],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
     )
     for line in hrzn_process.stdout:
